969f09c8a86efef2c8613c046e60705e0c095e35
keyzyashwant <yashwant6002@gmail.com>
2026-05-30 09:35:41 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ entry_points={
         # Remove or comment out:
         # 'imu_publisher = imu_sim.imu_publisher:main',
 
-        # Add this instead:
+        
         'imu_serial_node = imu_sim.imu_serial_node:main',
-        'kalman_node     = imu_sim.kalman_node:main',    # unchanged
+        'kalman_node     = imu_sim.kalman_node:main',    
     ],
 },
